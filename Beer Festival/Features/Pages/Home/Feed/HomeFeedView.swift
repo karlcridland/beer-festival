2daa14c46987a9bb76448fc8e16e3611f7a0dbc1
Karl Cridland <karlcridland@gmail.com>
2025-09-30 23:46:05 +0100

    Changes to home view set up.

--- Beer Festival/Features/Pages/Home/Feed/HomeFeedView.swift
+++ Beer Festival/Features/Pages/Home/Feed/HomeFeedView.swift
@@ -24,6 +24,7 @@ struct HomeFeedView: View {
             }
             .frame(maxWidth: .infinity)
         }
+        .background(.backgroundYellow)
     }
     
 }
--- Beer Festival/Features/Pages/Home/HomeView.swift
+++ Beer Festival/Features/Pages/Home/HomeView.swift
@@ -6,64 +6,79 @@
 //
 
 import SwiftUI
-import CoreData
 
 @available(iOS 26.0, *)
 struct HomeView: View {
     
-    @Environment(\.managedObjectContext) private var viewContext
-    
+    @ObservedObject var viewModel: HomeViewModel
     @State var search: String = ""
     @FocusState private var isSearching: Bool
+    
+    init() {
+        _viewModel = ObservedObject(initialValue: HomeViewModel())
+    }
 
     var body: some View {
         NavigationStack {
-            HomeFeedView()
-                .background(.backgroundYellow)
-                .toolbar {
-                    
-                    // Bottom left toolbar - displays when not searching
-                    
-                    if (!isSearching) {
-                        ToolbarItem(placement: .bottomBar) {
-                            Button("Filter", systemImage: "line.3.horizontal.decrease") {
-                                
-                            }
-                        }
-                        ToolbarSpacer(placement: .bottomBar)
-                    }
-                    
-                    // Bottom center toolbar - displays always but shifts side when in use/not in use
-                    
-                    ToolbarItem(id: "Search Bar", placement: .bottomBar) {
-                        TextField("Search", text: $search)
-                            .focused($isSearching)
-                            .padding(.horizontal, 10)
-                            .submitLabel(.search)
-                            .onSubmit {
-                                isSearching = false
-                            }
-                    }
-                    
-                    // Bottom right toolbar - displays when searching to cancel search
-                    
-                    if (isSearching) {
-                        ToolbarSpacer(placement: .bottomBar)
-                        ToolbarItem(placement: .bottomBar) {
-                            Button("Close", systemImage: "xmark") {
-                                isSearching = false
-                            }
+            VStack {
+                HomeFeedView()
+            }
+            .id(isSearching)
+            .toolbar {
+                
+                // Bottom left toolbar - displays when not searching
+                
+                if (!isSearching) {
+                    ToolbarItem(placement: .bottomBar) {
+                        Button("Filter", systemImage: "line.3.horizontal.decrease") {
+                            
                         }
                     }
-                    
-                    // Top right navigation - option menu adding dropdowns to helpful pages
-                    
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        HomeMenuView(destinations: [.profile, .settings])
-                    }
-                    
+                    ToolbarSpacer(placement: .bottomBar)
                 }
+                
+                // Bottom center toolbar - displays always but shifts side when in use/not in use
+                
+                ToolbarItem(id: "Search Bar", placement: .bottomBar) {
+                    TextField("Search", text: $search)
+                        .focused($isSearching)
+                        .padding(.horizontal, 10)
+                        .submitLabel(.search)
+                        .onSubmit {
+                            isSearching = false
+                        }
+                }
+                
+                // Bottom right toolbar - displays when searching to cancel search
+                
+                if (isSearching) {
+                    ToolbarSpacer(placement: .bottomBar)
+                    ToolbarItem(placement: .bottomBar) {
+                        Button("Close", systemImage: "xmark") {
+                            isSearching = false
+                        }
+                    }
+                }
+                
+                // Top right navigation - option menu adding dropdowns to helpful pages
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HomeMenuView(destinations: [.profile, .settings])
+                }
+                
+            }
+            
         }
+    }
+    
+}
+
+class HomeViewModel: ObservableObject {
+    
+    var refresh_id: String = UUID().uuidString
+    
+    func refresh() {
+        refresh_id = UUID().uuidString
     }
     
 }
